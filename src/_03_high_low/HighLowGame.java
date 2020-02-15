a7028fb72b814e9cad078dfe943769978eb53bde
ilabstaff <ilabstaff@172.16.116.79>
2020-02-15 11:30:14 -0800

    shiny

--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -22,7 +22,7 @@ public class HighLowGame {
 			// 1. Ask the user for a guess using a pop-up window, and save their response
 		String input=JOptionPane.showInputDialog("guess a number");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-		int guess=Integer.parseInt(input);
+		int guess=Integer.parseInt(input); 
 				// 5. if the guess is correct
 		
 				// 6. Win
--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -15,9 +15,12 @@ public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
 
-JOptionPane.showInputDialog("how many shiny objects do you want");
+		String input=JOptionPane.showInputDialog("how many shiny objects do you want");
+		int times=Integer.parseInt(input);
 		// 3. Play the sound that many times
-
+for (int i = 0; i < ; i++) {
+	
+}
 		// 1. Call the method below
 		playMisterZee();
 	}
