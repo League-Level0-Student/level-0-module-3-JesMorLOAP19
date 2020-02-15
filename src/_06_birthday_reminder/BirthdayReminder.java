224d777e4cd06ee38320cdd0595eaa695eddadfb
ilabstaff <ilabstaff@172.16.116.79>
2020-02-15 11:29:52 -0800

    shiny objects

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -13,7 +13,7 @@ public class BirthdayReminder {
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "July 1st";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
 	String birthday=JOptionPane.showInputDialog(null,"what birthday do you want");
@@ -23,13 +23,24 @@ public class BirthdayReminder {
 		
 		// 4. if user asked for "mom"
 		if (birthday.equals("mom")) {
-			JOptionPane.showMessageDialog(null,""
+			JOptionPane.showMessageDialog(null,momsBirthday);
+		}
 			//print mom's birthday
 		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
+		else if (birthday.equals("dad")) {
+			JOptionPane.showMessageDialog(null,dadsBirthday);
+			}
+				// print dad's birthday
+			// 6. if user asked for your name
+		else if (birthday.equals("jessica")) {
+				JOptionPane.showMessageDialog(null,myBirthday);
+			}
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
-	} 
+		else {
+			
+		JOptionPane.showMessageDialog(null,"Sorry, i don't remember that person's birthday!");
+		}
+}
 }
---
+++ src/_07_years_alive/YearsAlive.java
@@ -0,0 +1,12 @@
+package _07_years_alive;
+
+public class YearsAlive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for (int i = 2009; i <2021 ; i++) {
+				System.out.println(i);
+			}
+	}
+
+}
